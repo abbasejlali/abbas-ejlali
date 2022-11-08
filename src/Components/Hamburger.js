759245a3627d53b1f0497f2098754c22331dd588
abbas ejlali <abbas.ejlali18@gmail.com>
2022-11-08 12:26:35 -0800

    bilud Hamberguer Menu v2

--- src/Components/Hamburger.js
+++ src/Components/Hamburger.js
@@ -2,15 +2,52 @@ import React, { Component } from "react";
 import Header from "./Header";
 import styles from "./Hamburger.module.css";
 class Hamburger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+  blockFunction = () => {
+    this.setState((prevprops) => ({
+      open: !prevprops.open,
+    }));
+  };
   render() {
     return (
       <>
-        <div className={styles.divmain}>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div
+          onClick={this.blockFunction}
+          style={
+            this.state.open
+              ? { top: "20px", left: "219px" }
+              : { top: "34px", left: "30px" }
+          }
+          className={styles.divmain}
+        >
+          <div
+            style={
+              this.state.open
+                ? { transform: "rotate(-45deg)", transformOrigin: "50px" }
+                : { transform: "rotate(0)" }
+            }
+          ></div>
+          <div
+            style={
+              this.state.open
+                ? { translatex: "-100%", opacity: "0" }
+                : { translatex: "0", opacity: "1" }
+            }
+          ></div>
+          <div
+            style={
+              this.state.open
+                ? { transform: "rotate(45deg)", transformOrigin: "48px" }
+                : { transform: "rotate(0)" }
+            }
+          ></div>
         </div>
-        <Header />
+        <Header open={this.state.open} />
       </>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,29 +3,56 @@ import Logo from "../images/Pasion..png";
 import styles from "./Header.module.css";
 class Header extends Component {
   render() {
+    const { open } = this.props;
     return (
-      <header className={styles.header}>
-        <nav>
-          <ul>
-            <li>
-              <a link="#">Home</a>
-            </li>
-            <li>
-              <a link="#">About Me</a>
-            </li>
-            <li>
-              <a link="#">Portfolio</a>
-            </li>
-            <li>
-              <a link="#">Contact Me</a>
-            </li>
-            <li>
-              <a link="#">Blog</a>
-            </li>
-          </ul>
-        </nav>
-        <img src={Logo} alt="Logo" />
-      </header>
+      <>
+        <header className={styles.header}>
+          <nav>
+            <ul>
+              <li>
+                <a link="#">Home</a>
+              </li>
+              <li>
+                <a link="#">About Me</a>
+              </li>
+              <li>
+                <a link="#">Portfolio</a>
+              </li>
+              <li>
+                <a link="#">Contact Me</a>
+              </li>
+              <li>
+                <a link="#">Blog</a>
+              </li>
+            </ul>
+          </nav>
+          <img src={Logo} alt="Logo" />
+        </header>
+        <div
+          className={styles.hambergurmenu}
+          style={open ? { left: "0" } : { left: "-100%" }}
+        >
+          <nav>
+            <ul>
+              <li>
+                <a link="#">Home</a>
+              </li>
+              <li>
+                <a link="#">About Me</a>
+              </li>
+              <li>
+                <a link="#">Portfolio</a>
+              </li>
+              <li>
+                <a link="#">Contact Me</a>
+              </li>
+              <li>
+                <a link="#">Blog</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </>
     );
   }
 }
