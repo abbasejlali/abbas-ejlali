4ff60e6a06f743512f91559dc9a68a10095b453c
abbas ejlali <abbas.ejlali18@gmail.com>
2022-11-09 10:17:02 -0800

    styled Banner.js and add font Rufina-bold

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Hamburger from "./Components/Hamburger";
 import Banner from "./Components/Banner";
+import "./index.css";
 class App extends Component {
   render() {
     return (
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import styles from "./Banner.module.css";
 import bannerimg from "../images/Untitled-3.png";
+import "../index.css";
+
 class Banner extends Component {
   render() {
     return (
       <div className={styles.Bannermain}>
-        <div className={styles.imgdiv}>
-          <img src={bannerimg} alt="bannerimage" />
-        </div>
         <div className={styles.headerdiv}>
-          <h1>
-            I’m Programmer <br />
-            Abbas Ejlali
-          </h1>
+          <h1>I’m Programmer Abbas Ejlali</h1>
 
           <p>
             I am Abbas Ejlali and I am glad that you came to my site I started
             programming in 1399 and studied IT at Shamsipour University in
             Tehran and currently I am a front and web programmer.
           </p>
-          <p>متن مخفی</p>
+          {/* <p>متن مخفی</p> */}
           <button>Read More</button>
         </div>
+
+        <img src={bannerimg} alt="bannerimage" />
       </div>
     );
   }
