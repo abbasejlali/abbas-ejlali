ef6e799bc1f1df07cfa056215c8f526d02e8bb87
abbas ejlali <abbas.ejlali18@gmail.com>
2022-11-09 10:36:43 -0800

    use react to Banner.js for button Read more

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,6 +4,18 @@ import bannerimg from "../images/Untitled-3.png";
 import "../index.css";
 
 class Banner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      more: false,
+    };
+  }
+  ClickHandeler = () => {
+    this.setState((prevstate) => ({
+      more: !prevstate.more,
+    }));
+    console.log(this.state.more);
+  };
   render() {
     return (
       <div className={styles.Bannermain}>
@@ -14,9 +26,19 @@ class Banner extends Component {
             I am Abbas Ejlali and I am glad that you came to my site I started
             programming in 1399 and studied IT at Shamsipour University in
             Tehran and currently I am a front and web programmer.
+            <span
+              style={
+                this.state.more ? { display: "flex" } : { display: "none" }
+              }
+            >
+              I am interested in various topics in computer science, such as web
+              programming, network or even robotics, but the languages ​​and
+              libraries I know include: Html Css Bootstrap Java Script React Git
+              illustrator
+            </span>
           </p>
           {/* <p>متن مخفی</p> */}
-          <button>Read More</button>
+          <button onClick={this.ClickHandeler}>Read More</button>
         </div>
 
         <img src={bannerimg} alt="bannerimage" />
